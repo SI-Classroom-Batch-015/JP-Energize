85ba7a19c7337d6d9b9c963d472f4614b9c4cb1e
phillipwilke <phillipwilke1@web.de>
2024-09-23 09:52:45 +0200

    create HomeChart outsourced

---
+++ JP Energize/JP Energize/Charts/HomeChart.swift
@@ -0,0 +1,18 @@
+//
+//  HomeChart.swift
+//  JP Energize
+//
+//  Created by Phillip Wilke on 19.09.24.
+//
+
+import SwiftUI
+
+struct HomeChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeChart()
+}
---
+++ JP Energize/JP Energize/Enum/HomeViewPicker.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewPicker.swift
+//  JP Energize
+//
+//  Created by Phillip Wilke on 19.09.24.
+//
+
+import Foundation
+
+
+enum HomePicker: String, CaseIterable, Codable {
+    case week, month, year
+}
