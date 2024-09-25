9d4db91ee8ed240cb9982be5f8c15269be82c973
phillipwilke <phillipwilke1@web.de>
2024-09-25 21:40:31 +0200

    create PanelDetailView

--- JP Energize/JP Energize/Views/PanelDetailView.swift
+++ JP Energize/JP Energize/Views/PanelDetailView.swift
@@ -8,11 +8,67 @@
 import SwiftUI
 
 struct PanelDetailView: View {
+    
+    @ObservedObject private var viewModel = InverterViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Form {
+            
+      
+                
+                Section{
+                    HStack(alignment: .center) {
+                        
+                        Text("Status: Active")
+                            .foregroundStyle(.green)
+                            .font(.caption)
+                        
+                        Spacer()
+                        
+                        Text("Solar Panel 1")
+                            .font(.headline)
+                    }
+                    
+                    VStack(alignment: .center){
+                        Image("solarpanel")
+                            .resizable()
+                            .imageScale(.medium)
+                            .frame(width: 250, height: 250)
+                        
+                        Text("Max Power Output: 600W")
+                           
+                    }
+                }
+            
+            Section{
+                    VStack(alignment: .center) {
+                        
+                            Text("Standort:")
+                                .foregroundStyle(.gray)
+                            Text(viewModel.inverter?.station_info?.state ?? "Geht nicht")
+                            
+                        
+                        
+                        Text("Version:")
+                            .foregroundStyle(.gray)
+                        Text(viewModel.inverter?.version ?? "Geht nicht")
+                        
+                        Text("Azimutwinkel:")
+                            .foregroundStyle(.gray)
+                        
+                        Text(viewModel.inverter?.inputs?.azimuth ?? "Geht nicht")
+                        Text("180 Grad = Südlich ausgerichtet")
+                            .foregroundStyle(.gray)
+                    }
+                    .padding()
+                }
+         
+        }
     }
 }
 
 #Preview {
     PanelDetailView()
 }
+
