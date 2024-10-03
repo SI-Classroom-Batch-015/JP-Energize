c46c550cf6319bb9c4b6ec9555030a9a6fd06731
phillipwilke <phillipwilke1@web.de>
2024-10-03 17:41:43 +0200

    create PersistentStore

--- JP Energize/JP Energize/CoreData/PersistentStore.swift
+++ JP Energize/JP Energize/CoreData/PersistentStore.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import CoreData
+
+struct PersistentStore {
+    
+    static let shared = PersistentStore()
+    
+    init() {
+        container = NSPersistentContainer(name: "Core Data")
+        
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Error with Core Data: \(error) ,\(error.userInfo)")
+            }
+        }
+    }
+    
+    
+    private let container: NSPersistentContainer
+    var context: NSManagedObjectContext { container.viewContext }
+    
+    
+    func save() {
+        guard context.hasChanges else { return }
+        
+        do {
+            try context.save()
+        } catch let error as NSError {
+            NSLog("Unresolved error saving context: \(error), \(error.userInfo)")
+        }
+    }
+    
+}
+
+
