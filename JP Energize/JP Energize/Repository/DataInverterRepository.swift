7146e96812766eff07d62e55a2310d064fe3408e
phillipwilke <phillipwilke1@web.de>
2024-10-03 17:42:05 +0200

    change Name Inverter to InverterModel

--- JP Energize/JP Energize/Repository/DataInverterRepository.swift
+++ JP Energize/JP Energize/Repository/DataInverterRepository.swift
@@ -22,7 +22,7 @@ class DataInverterRepository {
     
 
     
-    func fetchInverter() async throws -> Inverter {
+    func fetchInverter() async throws -> InverterModel {
    // let envKey =
     guard let url = URL(string: "https://developer.nrel.gov/api/pvwatts/v8.json?api_key=tWh7ebcIAiAPnUVUBg4ISsEq1R7j0V0G4IvcIBJo&azimuth=180&system_capacity=4&losses=14&array_type=1&module_type=0&gcr=0.4&dc_ac_ratio=1.2&inv_eff=96.0&radius=0&dataset=nsrdb&tilt=10&address=boulder, co&soiling=12|4|45|23|9|99|67|12.54|54|9|0|7.6&albedo=0.3&bifaciality=0.7") else {
       throw URLError(.badURL)
@@ -34,7 +34,7 @@ class DataInverterRepository {
       guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
         throw URLError(.badServerResponse)
       }
-      let inverterResponse = try JSONDecoder().decode(Inverter.self, from: data)
+      let inverterResponse = try JSONDecoder().decode(InverterModel.self, from: data)
         return inverterResponse
     } catch {
       print("Fetch articles error: \(error)")
