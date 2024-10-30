afac5649b323b6c1a86d0c3e38e6fca28072131c
phillipwilke <phillipwilke1@web.de>
2024-10-30 16:30:16 +0100

    create ForgotPasswordView

---
+++ JP Energize/JP Energize/Views/ForgotPasswordView.swift
@@ -0,0 +1,19 @@
+//
+//  ForgotPasswordView.swift
+//  JP Energize
+//
+//  Created by Phillip Wilke on 28.10.24.
+//
+
+import SwiftUI
+import Firebase
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotPasswordView()
+}
