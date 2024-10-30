0f98d4235c6d447db289edf182604ae1b06f385f
phillipwilke <phillipwilke1@web.de>
2024-10-30 16:28:24 +0100

    change name UserViewModel to AuthViewModel

---
+++ JP Energize/JP Energize/ViewModel/AuthViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  UserViewModel.swift
+//  JP Energize
+//
+//  Created by Phillip Wilke on 27.09.24.
+//
+
+import Foundation
+import Observation
+
+
+
+@Observable
+class AuthViewModel {
+    
+    func signUp(email: String, password: String, firstName: String, lastName: String) async {
+        do {
+            try await FirebaseAuthManager.shared.signUp(email: email, password: password, firstName: firstName, lastName: lastName)
+        } catch {
+            print("Error signUp!\(error.localizedDescription)")
+        }
+    }
+    
+    func signIn(email: String, password: String) async {
+        do {
+            try await FirebaseAuthManager.shared.signIn(email: email, password: password)
+        } catch {
+            print("Error signIn!\(error.localizedDescription)")
+        }
+    }
+    
+    func signOut(email: String, password: String) async {
+        
+        FirebaseAuthManager.shared.signOut()
+        
+    }
+    
+    
+}
