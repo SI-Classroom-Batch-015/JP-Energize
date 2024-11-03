3e2349d72f5e52f220efb3f53d0d39f3400892d2
phillipwilke <phillipwilke1@web.de>
2024-11-04 00:37:35 +0100

    add enviroment dismiss and change userViewModel to viewModel

--- JP Energize/JP Energize/Sheets/SignUpSheet.swift
+++ JP Energize/JP Energize/Sheets/SignUpSheet.swift
@@ -12,92 +12,101 @@ struct SignUpSheet: View {
     @State var password = ""
     @State var firstName = ""
     @State var lastName = ""
-    @State var userViewModel = AuthViewModel()
+    @Bindable var viewModel: AuthViewModel
     @State var isPresenting = false
-    @Environment(\.dismiss) private var dismiss
-    
-    var body: some View {
-        VStack {
-            HStack {
-                Button(action: {
-                    dismiss()
-                }) {
-                    Image(systemName: "xmark")
-                        .foregroundColor(.gray)
-                        .padding()
-                }
-                Spacer()
-                
-              
-            }
-            
-            Image("startpanel")
-                .resizable()
-                .scaledToFit()
-                .frame(height: 300)
-                .clipped()
-                .ignoresSafeArea(edges: .top)
-            
-            Spacer()
-            
-            Text("Registration")
-                .font(.title)
-             
-                .foregroundStyle(.gray)
-            
+        @Environment(\.dismiss) private var dismiss
+
+        var body: some View {
             VStack {
-                TextField("Email", text: $email)
-                    .padding()
-                    .background(Color(.secondarySystemBackground))
-                    .cornerRadius(10)
-                    .padding(.horizontal, 20)
-                
-                SecureField("Password", text: $password)
-                    .padding()
-                    .background(Color(.secondarySystemBackground))
-                    .cornerRadius(10)
-                    .padding(.horizontal, 20)
-                
-                TextField("firstName", text: $firstName)
-                    .padding()
-                    .background(Color(.secondarySystemBackground))
-                    .cornerRadius(10)
-                    .padding(.horizontal, 20)
-                
-                TextField("lastName", text: $lastName)
-                    .padding()
-                    .background(Color(.secondarySystemBackground))
-                    .cornerRadius(10)
-                    .padding(.horizontal, 20)
-                
-                
-                
-                Button("SignUp") {
-                    attemptSignUp()
-                    dismiss()
+                HStack {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        Image(systemName: "xmark")
+                            .foregroundColor(.gray)
+                            .padding()
+                    }
+                    Spacer()
                 }
-                .fontWeight(.bold)
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(Color.green)
-                .foregroundColor(.white)
-                .cornerRadius(10)
-                .padding(.top, 20)
-                .padding(.horizontal, 20)
-                
+
+                Image("startpanel")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 300)
+                    .clipped()
+                    .ignoresSafeArea(edges: .top)
+
                 Spacer()
+
+                Text("Registration")
+                    .font(.title)
+                    .foregroundStyle(.gray)
+
+                VStack {
+                    TextField("Email", text: $email)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 20)
+                      
+
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 20)
+
+                    TextField("First Name", text: $firstName)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 20)
+
+                    TextField("Last Name", text: $lastName)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 20)
+
+                    Button("Sign Up") {
+                        attemptSignUp()
+                    }
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(firstName.isEmpty || lastName.isEmpty ? Color.gray : Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .padding(.top, 20)
+                    .padding(.horizontal, 20)
+                    .disabled(firstName.isEmpty || lastName.isEmpty)
+
+                    Spacer()
+                }
+                .padding(.horizontal, 30)
+                .autocapitalization(.none)
+
+                .alert(isPresented: $viewModel.showAlert) {
+                    Alert(
+                        title: Text("Info"),
+                        message: Text(viewModel.alertMessage),
+                        dismissButton: .default(Text("OK")) {
+                            if viewModel.alertMessage.contains("erfolgreich") {
+                                dismiss() 
+                            }
+                        }
+                    )
+                }
             }
-            .padding(.horizontal, 30)
+        }
+
+        func attemptSignUp() {
+            Task {
+                await viewModel.signUp(email: email, password: password, firstName: firstName, lastName: lastName)
+            }
         }
     }
-    
-    func attemptSignUp() {
-        Task {
-            await userViewModel.signUp(email: email, password: password, firstName: firstName, lastName: lastName)
-        }
-    }
-}
 
 #Preview {
-    SignUpSheet()
+    SignUpSheet(viewModel: AuthViewModel())
 }
