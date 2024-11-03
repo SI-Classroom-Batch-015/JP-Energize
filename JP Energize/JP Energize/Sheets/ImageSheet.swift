25e8f73224ce24db81004778192087186a480d84
phillipwilke <phillipwilke1@web.de>
2024-11-04 00:35:36 +0100

    add dismiss and sheetShowing

--- JP Energize/JP Energize/Sheets/ImageSheet.swift
+++ JP Energize/JP Energize/Sheets/ImageSheet.swift
@@ -13,41 +13,45 @@ struct ImageSheet: View {
     @Binding var selectedImage: UIImage?
     @Binding var sheetShowing: Bool
     @State var showImagePicker: Bool = false
-    
+    @Environment(\.dismiss) var dismiss  // Dismiss-Umgebung für das Schließen der Ansicht
+
     var body: some View {
-        
         VStack {
-            if let image =  selectedImage {
+            if let image = selectedImage {
                 Image(uiImage: image)
                     .resizable()
                     .scaledToFill()
                     .frame(width: 100, height: 100)
                     .clipShape(Circle())
             }
-            
+
             Button {
                 showImagePicker = true
             } label: {
                 Text("Wähle ein Foto aus")
                     .foregroundStyle(.black)
             }
-            
+
             if let selectedImage = selectedImage {
                 Button {
                     FirebaseStorageManager.shared.uploadImageToFirebase(image: selectedImage)
                     DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
-                        sheetShowing.toggle()
+                        dismiss()  // Sheet nach dem Hochladen schließen
                     }
-                    
                 } label: {
                     Text("Foto hochladen")
-                    
                 }
             }
-            
+
             Divider()
             
-        
+            // Abbrechen-Button
+            Button {
+                dismiss()  // Sheet ohne Auswahl schließen
+            } label: {
+                Text("Abbrechen")
+                    .foregroundColor(.red)
+            }
         }
         .sheet(isPresented: $showImagePicker) {
             ImagePicker(selectedImage: $selectedImage, isPickerShowing: $showImagePicker)
@@ -58,3 +62,4 @@ struct ImageSheet: View {
 #Preview {
     ImageSheet(selectedImage: .constant(.thor), sheetShowing: .constant(false))
 }
+
